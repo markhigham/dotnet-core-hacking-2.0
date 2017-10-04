bed4ec2cedae2bec422fdd67827e650cc2a26d5b
Mark Higham <mark.higham@dxc.com>
2017-10-04 13:15:17 +0100

    initial files

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace tel
+{
+
+    public class TestClass
+    {
+        public string StringValue { get; set; }
+        public int NumericValue { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var o = new TestClass
+            {
+                StringValue = "This is a string value"
+            };
+
+            var json = JsonConvert.SerializeObject(o, Formatting.Indented);
+            Console.WriteLine(json);
+
+        }
+    }
+}
